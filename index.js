e9e3b52f8ee0afc8ed3b03b5d8ffca4bd49fc085
ernest-rzant <ernestorodriguez643@gmail.com>
2021-11-11 08:52:07 -0600

    S25 Procesando Formulario

--- index.js
+++ index.js
@@ -21,15 +21,66 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'text/html');
 
         // 2. Escribiendo la respuesta
-        res.write("<html>");
-        res.write("<head><title>My App</title></head>");
-        res.write(`<body><h1>Hello from the server ;)</h1></body>`);
-        res.write("</html>");
+        res.write(`
+        <html>
+            <head>
+                <title>Enter message</title>
+            </head>
+            <body>
+                <h1>Send Message</h1>
+                <form action="/message" method="POST">
+                    <input type"text" name="message">
+                    <button type="submit">Send</button>
+                </form>
+            </body>
+        </html>
+        `);
 
         // Cerrando conexion
         res.end();
         
-    }else if(url === '/author'){
+    }else if (url === '/message' && method === "POST"){
+        // 1.- Se crea una variable para guardar los datos de entrada
+        let body = [];
+
+        // 2.- Registrar un manejador para la entrada de los datos
+        req.on("data", (chunk) => {
+            // 2.1.- Registrando los trozos que llegan al backend
+            console.log(chunk);
+
+            // 2.2.- Acumulo los datos de entrada
+            body.push(chunk);
+
+            // 2.3.- Proteccion en caso de recepcion masiva de datos
+            if (body.length > 1e6) req.socket.destroy();
+        })
+        // 3.- REgistrando un manejador de fin de recepcion de datos
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            
+            // Estableciendo el tipo de retorno
+            // como HTML
+            res.setHeader('Content-Type', 'text/html');
+            res.write(`
+            <html>
+                <head>
+                <title>Received message</title>
+                </head>
+            <body>
+                <h1>Received Message</h1>
+                <p>Thank you!!</p>
+                <p>The mesaage we received was this: ${message}</p>
+            </body>
+            </html>
+            `);
+
+            // Finalizo la conexion
+            return res.end();
+            
+        })
+
+    } else if(url === '/author'){
         // Respuesta ante "Get /"    
         // 1. Estableciendo el tipo de retorno    
         // como HTML    
@@ -61,7 +112,7 @@ const server = http.createServer((req, res) => {
         // Escribiendo la respuesta
         res.write("<html>");
         res.write("<head><title>My App</title></head>");
-        res.write(`<body><h1>Error: 404 ~ Recurso no enconmtrado</h1></body>`);
+        res.write(`<body><h1>Error: 404 ~ Recurso no encontrado</h1></body>`);
         res.write("</html>");
 
         // Cerrando conexion
@@ -74,5 +125,5 @@ const server = http.createServer((req, res) => {
 // cuando el servidor este escuchando
 // Mi IP 192.168.1.66:3000
 server.listen(3000, '0.0.0.0', () => {
-    console.log("Servidor escuchando en http://localhost:3000");
+    console.log("Servidor escuchando en http://0.0.0.0:3000");
 });
