a5e3cc48350fc921f7d332cb192820f9572e6530
ernest-rzant <ernestorodriguez643@gmail.com>
2021-11-23 08:35:45 -0600

    S30 Rutas multiples

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ import Express from 'express';
 // Crear instancia de Express
 const app = Express();  // (req, res, next) => {} event handler
 
+// Se debe colocar primero  ya que el orden de registro
+// determina el orden de verificacion
 app.use('/about', (_, res) => {
     // Registrar un mensaje en el log
     console.log('Se ha realizado la peticion: "/about"');
     res.send("<h1>Acerca de ...</h1>\n Sitio inicial hecho con NodeJs");
 });
 
-// Registrando el tercer middleware
-app.use('/', (_, res) => {
+// La ruta raiz entra en todo tipo de peticion
+app.use(['/', '/home'], (_, res) => {
     // Registrar un mensaje en el log
     console.log('Se ha realizado la peticion: "/"');
     res.send("<h1>Mi App</h1>\n Bienvenido a este sitio");
