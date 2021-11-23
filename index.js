34b4aade538e430b31326c80fbe6d33d6f3ece30
ernest-rzant <ernestorodriguez643@gmail.com>
2021-11-23 08:24:01 -0600

    S30 Administrando diferentes rutas

--- index.js
+++ index.js
@@ -11,29 +11,18 @@ import Express from 'express';
 // Crear instancia de Express
 const app = Express();  // (req, res, next) => {} event handler
 
-// Registrando el primer middleware
-app.use((req, res, next) => {
+app.use('/about', (_, res) => {
     // Registrar un mensaje en el log
-    console.log(" Estoy en el middleware 1");
-    // Dar la instruccion de pasar al siguiente middleware
-    next();
-})
-
-// Registrando el segundo middleware
-app.use((req, res, next) => {
-    // Registrar un mensaje en el log
-    console.log(" Estoy en el middleware 2");
-    // Dar la instruccion de pasar al siguiente middleware
-    next();
-})
+    console.log('Se ha realizado la peticion: "/about"');
+    res.send("<h1>Acerca de ...</h1>\n Sitio inicial hecho con NodeJs");
+});
 
 // Registrando el tercer middleware
-app.use((_, res) => {
+app.use('/', (_, res) => {
     // Registrar un mensaje en el log
-    console.log(" Estoy en el middleware 3");
-    console.log(" Emitiendo respuesta a cliente");
-    res.send("<h1>Mi respuesta</h1>\n Hola");
-})
+    console.log('Se ha realizado la peticion: "/"');
+    res.send("<h1>Mi App</h1>\n Bienvenido a este sitio");
+});
 
 // Poniendo a escuchar la app express
 app.listen(3000, '0.0.0.0', () => {
